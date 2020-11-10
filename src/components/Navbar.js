7cb192432fc25938c13e943838e394d6235d0c2d
nuzulzen <nuzulzen@gmail.com>
2020-11-11 06:57:23 +0700

    add activeLinkclass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,12 @@ function Navbar() {
   return (
     <div className="navbar container  ">
       <div className="nav-center bgColor5 d-flex align-content-center justify-content-between">
-        <Link to="/">Cocktail io</Link>
-        <Link to="/about">About</Link>
+        <NavLink activeClassName="active " to="/dashboard">
+          Cocktail io
+        </NavLink>
+        <NavLink activeClassName="active" to="/about">
+          About
+        </NavLink>
       </div>
     </div>
   );
