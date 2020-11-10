3cfe9f55d8ec84b817671c263bac3808429bc396
nuzulzen <nuzulzen@gmail.com>
2020-11-11 04:17:27 +0700

    add bootstrap class

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 function Cocktail({ name, image, id, info, glass }) {
   return (
-    <div className="cocktail">
-      <img src={image} alt={name} />
-      <div className="cocktail-info">
-        <h3>{name}</h3>
-        <h4>{glass}</h4>
-      </div>
+    <div className="cocktail bgColor3 d-flex mb-2 ">
+      <Row>
+        <Col xs={4} md={4}>
+          <img className="img-fluid rounded " src={image} alt={name} />
+        </Col>
+        <div className="cocktail-info p-1">
+          <h3>{name}</h3>
+          <h4>{glass}</h4>
+          <h5>{info}</h5>
+        </div>
+      </Row>
     </div>
   );
 }
