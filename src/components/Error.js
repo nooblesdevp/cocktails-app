03d61827b9793de8b31f9ae831f7d341ab9a1113
nuzulzen <nuzulzen@gmail.com>
2020-11-11 08:42:03 +0700

    fix error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Error() {
   return (
-    <div className="error-page">
-      <div className="error-container">
-        <h1>opps! it's a dead end</h1>
-        <Link to="/">back home</Link>
-      </div>
-      <h1>error</h1>
+    <div className="error-page container bgColor3">
+      {["Info"].map((variant, idx) => (
+        <Card
+          bg={variant.toLowerCase()}
+          key={idx}
+          text={variant.toLowerCase() === "light" ? "dark" : "white"}
+          className="error-page-card"
+        >
+          <Card.Header>Ops !</Card.Header>
+          <Card.Body>
+            <Card.Text>Page not found :(</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+
+      <Navbar />
     </div>
   );
 }
